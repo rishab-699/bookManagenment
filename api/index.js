56b52afa5a08e89f62ad0f6e6711cdfa044cbfd7
Rishab Jain <rishabjainrj2001@gmail.com>
2024-10-01 17:39:59 +0530

    vercel config

--- api/index.js
+++ api/index.js
@@ -9,14 +9,19 @@ const TransactionRoute  = require('./routes/booktransaction');
 dotenv.config()
 const app = express();
 const port = process.env.PORT;
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST","GET","PUT"],
+    credentials: true
+}
+));
 
 mongoose.connect(process.env.MONGO_URL, {dbName: process.env.dbName})
 .then(console.log('Database Connected'))
 .catch((err)=> console.log(err));
 
 app.get("/",(req,res)=>{
-    res.send('Hello!');
+    res.json('Hello!');
 })
 
 app.use(express.json());
